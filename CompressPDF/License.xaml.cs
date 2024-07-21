04ba2c1fa34c0ce90f52f9e83aee4b97553efd49
Petr Grebenicek <petrgrebenicek@outlook.com>
2024-07-21 02:37:20 +0200

    fixed links

--- CompressPDF/License.xaml.cs
+++ CompressPDF/License.xaml.cs
@@ -16,5 +16,23 @@ namespace CompressPDF
         {
             this.Close();
         }
+
+        private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
+        {
+            try
+            {
+                System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo
+                {
+                    FileName = e.Uri.AbsoluteUri,
+                    UseShellExecute = true
+                };
+                System.Diagnostics.Process.Start(psi);
+            }
+            catch
+            {
+            }
+
+            e.Handled = true;
+        }
     }
 }
