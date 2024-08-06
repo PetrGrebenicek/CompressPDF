0336b77c2ffb16476123255cfbb410971ed46a7f
Petr Grebenicek <petrgrebenicek@outlook.com>
2024-08-06 17:44:47 +0200

    fixed ui bug with license.xaml opening position

--- CompressPDF/License.xaml.cs
+++ CompressPDF/License.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Interop;
 
 namespace CompressPDF
 {
@@ -10,6 +12,41 @@ namespace CompressPDF
         public License()
         {
             InitializeComponent();
+            this.Loaded += License_Loaded;
+        }
+
+        private void License_Loaded(object sender, RoutedEventArgs e)
+        {
+            AdjustWindowPosition();
+        }
+
+        private void AdjustWindowPosition()
+        {
+            var workingArea = SystemParameters.WorkArea;
+
+            // Retrieve window dimensions
+            var windowWidth = this.ActualWidth;
+            var windowHeight = this.ActualHeight;
+
+            // Check and adjust left position
+            if (this.Left < workingArea.Left)
+            {
+                this.Left = workingArea.Left;
+            }
+            else if (this.Left + windowWidth > workingArea.Right)
+            {
+                this.Left = workingArea.Right - windowWidth;
+            }
+
+            // Check and adjust top position
+            if (this.Top < workingArea.Top)
+            {
+                this.Top = workingArea.Top;
+            }
+            else if (this.Top + windowHeight > workingArea.Bottom)
+            {
+                this.Top = workingArea.Bottom - windowHeight;
+            }
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
